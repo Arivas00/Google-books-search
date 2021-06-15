313bc258270e1a6e54c613f475e6ebc33332bb8f
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-06-15 10:20:54 -0400

    fixed navbar

--- client/src/components/NavbarComp/index.js
+++ client/src/components/NavbarComp/index.js
@@ -8,8 +8,8 @@ export default function NavbarComp() {
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="/">Google-Books-Search</Navbar.Brand>
         <Nav className="mr-auto">
-          {/* <Nav.Link href="/">Search</Nav.Link>
-          <Nav.Link href="/saved">Saved</Nav.Link> */}
+          <Nav.Link href="/">Search</Nav.Link>
+          <Nav.Link href="/saved">Saved</Nav.Link>
         </Nav>
       </Navbar>
     </div>
