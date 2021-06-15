2773e205893f26a34582f2ab5fb0ba8e033c5b6d
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-06-15 12:20:47 -0400

    fixing deploy again

--- routes/API/index.js
+++ routes/API/index.js
@@ -1,7 +1,11 @@
+const path = require("path")
 const router = require("express").Router();
 const bookAPIRoutes = require("./booksAPI");
 
 router.use("/booksAPI", bookAPIRoutes);
 
+router.use((req, res) =>
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 module.exports = router;
